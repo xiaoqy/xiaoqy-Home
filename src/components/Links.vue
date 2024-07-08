b90ce53be35c4651aed6e0dd65cf542f066eb0c3
Xiao Qingyu <ixiaoqy@gmail.com>
2024-07-09 00:06:28 +0800

    Update Links.vue

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -73,17 +73,23 @@ const siteIcon = {
 };
 
 // 链接跳转
-// const jumpLink = (data) => {
-//  if (data.name === "音乐" && store.musicClick) {
-//    if (typeof $openList === "function") $openList();
-//  } else {
-//    window.open(data.link, "_blank");
-//  }
-// };
+ const jumpLink = (data) => {
+   window.open(data.link, "_blank");
+  };
+/*
+ const jumpLink = (data) => {
+  if (data.name === "音乐" && store.musicClick) {
+    if (typeof $openList === "function") $openList();
+  } else {
+    window.open(data.link, "_blank");
+  }
+};
+*/
 
-// onMounted(() => {
-//  console.log(siteLinks);
-// });
+
+ onMounted(() => {
+  console.log(siteLinks);
+ });
 </script>
 
 <style lang="scss" scoped>
