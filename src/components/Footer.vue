98a677e58cb4b41debb58165cc5d4a9f71bf8aff
Xiao Qingyu <ixiaoqy@gmail.com>
2024-07-21 19:45:47 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -13,7 +13,7 @@
         </span>
         <!-- 以下信息请不要修改哦 -->
         <span class="hidden">
-          &amp;&nbsp;Made&nbsp;by xiaoqy
+          &amp; Powered by xiaoqy (白小七羽)
         </span>
         <!-- 站点备案 -->
         <a v-if="siteIcp" href="https://beian.miit.gov.cn" target="_blank">
