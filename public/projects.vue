9480eb1d3df2707bffc83bff9013b6dfadb9305b
Xiao Qingyu <ixiaoqy@gmail.com>
2024-07-26 00:11:43 +0800

    update

---
+++ public/projects.vue
@@ -0,0 +1,15 @@
+<template>
+  <div v-html="htmlContent"></div>
+</template>
+
+<script>
+import htmlContent from './projects.html'; // 导入HTML文件
+
+export default {
+  data() {
+    return {
+      htmlContent
+    };
+  }
+};
+</script>
